3d50526801d84128913ba5b1bc0bab04910ff70e
Onursoyar <soyaronur@gmail.com>
2022-06-24 12:00:28 +0000

    Create and update variables for the loop process

--- run.py
+++ run.py
@@ -56,10 +56,76 @@ username: ").capitalize()
     return user_name.name
 
 
+MENU = {"a","b","c"}
+
+
+def get_order():
+    print()
+    print("Hello and welcome, are you facing problems regarding our services? Choose an option below!")
+    print()
+    current_order = []
+    while True:
+        print("What do you need help with?")
+        print()
+        print("a) Account/Password, b) Software Error, c) Expired License")
+        order = input()
+        if order in MENU:
+            current_order.append(order)
+        else:
+            print("I'm sorry, we can't help you with that.")
+            continue
+        if order == "a":
+            print("Have you tried the below solutions? yes/no")
+            print()
+            print("a) Logging out and in again. b) Reset your password. c) Try logging in on another device/browser or app. d) Erasing browser cache.")
+
+        elif order == "b":
+            print("Have you tried the below solutions? yes/no")
+            print()
+            print("a) Restarting your computer? b) Delete and re-install the application? c) Did you give permission for the app on your settings? d) Use the Web version and see if it works?")  
+
+        elif order == "c":
+            print("Have you tried the below solutions? yes/no")
+            print()
+            print("a) Do you have a valid License? b) Have you cehcked if it expired? c) Can you use your account in another browser or app? d) Can you log in to your account?")        
+
+        choice = input()    
+        if choice == ("yes"):
+            print("Please contact our support desk.")
+            
+        elif choice == ("no"):   
+            print("Please try suggested solutions and comeback again.")
+
+            
+        else: 
+            raise Exception("invalid input")   
+            break
+
+        if is_order_complete():
+            return current_order
+
+def is_order_complete():
+    print("Anything else? yes/no")
+    choice = input()
+    if choice == "no":
+        return True
+    elif choice == "yes":
+        return False
+    else:
+        raise Exception("invalid input")
+
+def output_order(order_list):
+    print("Thank you for visiting us and have a great day")
+    for order in order_list:
+        print("Chosen ticket for support")
+        print(order) 
+
 def main():
     user_name()
-
+    order = get_order()
+    output_order(order)
     customer.append_row(customer_details)
+
 
 
 if __name__ == "__main__":
